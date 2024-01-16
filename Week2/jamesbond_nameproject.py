863b7a30ad204811e03c76b5f163d6f8a9941449
davidlunceford10 <davidlunceford10@yahoo.com>
2024-01-15 21:52:02 -0700

    created file

---
+++ Week2/jamesbond_nameproject.py
@@ -0,0 +1,5 @@
+first_name = input('What is your first name? ').capitalize()
+last_name = input('What is your last name? ').capitalize()
+print('')
+output = f'Your name is {last_name}, {first_name} {last_name}'
+print(output)

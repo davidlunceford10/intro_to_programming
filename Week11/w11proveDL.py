6d9ef63c3e62bf17d9f39ad8ca3e504a1e2980b4
davidlunceford10 <davidlunceford10@yahoo.com>
2024-03-23 20:16:24 -0600

    f

--- Week11/w11proveDL.py
+++ Week11/w11proveDL.py
@@ -1,2 +1,39 @@
-with open('week11/life-expectancy.csv') as lifenumbas:
-    
+country_list = []
+country_codes = []
+year_list = []
+life_expectancy_list = []
+
+
+with open('Week11/life-expectancy.txt') as lifedata:
+    for line in lifedata:
+        splitlifedataline = line.strip().split(',')
+        country = splitlifedataline[0]
+        country_list.append(country)
+        country_code = splitlifedataline[1]
+        country_codes.append(country_code)
+        year = splitlifedataline[2]
+        year_list.append(year)
+        life_expectancy_inyears = splitlifedataline[3]
+        life_expectancy_list.append(life_expectancy_inyears)
+
+    highest_life_expectancy = max(life_expectancy_list)
+    shortest_life_expectancy = min(life_expectancy_list)
+
+    print(f'The overall max life expectancy is: {highest_life_expectancy}')
+    print(f'The overall min life expectancy is: {shortest_life_expectancy}')
+
+
+        
+        
+
+
+
+
+# year_of_interest = int(input('Enter the year of interest: '))
+
+
+
+        
+        
+
+        
